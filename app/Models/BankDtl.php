049856901e3efe53d35f9ac7c55a08a8761240a4
subham-codesigma <subham.ghorui@codesigma.tech>
2022-08-06 12:51:38 +0530

    Test

---
+++ app/Models/BankDtl.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class BankDtl extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'BANK_CODE',
+        'BANK_NAME',
+        'A/C_HOLDER',
+    ];
+
+    protected $primaryKey = 'id';
+    protected $table = 'bank_dtl';
+
+    public function __construct()
+    {
+
+    }
+}
