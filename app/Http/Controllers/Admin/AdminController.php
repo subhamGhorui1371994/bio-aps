63d1125fa9bd0d1badbf9c0c9138f3f8bf289c34
subham-codesigma <subham.ghorui@codesigma.tech>
2022-08-12 23:17:40 +0530

    login fix 1.5h

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
+use Illuminate\Support\Facades\Session;
 
 class AdminController extends Controller
 {
