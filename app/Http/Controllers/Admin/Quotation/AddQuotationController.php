6a5c5a47c4bc8ad319d4fc3ef090bdc98b349393
bejsuman <bejsuman5@gmail.com>
2022-08-28 18:05:40 +0530

    show  Employee data

--- app/Http/Controllers/Admin/Quotation/AddQuotationController.php
+++ app/Http/Controllers/Admin/Quotation/AddQuotationController.php
@@ -2,18 +2,57 @@
 
 namespace App\Http\Controllers\Admin\Quotation;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AddQuotationController extends Controller
 {
-    
-    public function index(){
-        return view('admin.Quotation.add-quotation');
+
+    public function index()
+    {
+        $allCustomer = DB::table('employee_dtl')->pluck('EMPLOYEE_NAME', 'ID');
+        return view('admin.Quotation.add-quotation', compact('allCustomer'));
     }
 
-    public function addAddQuotation(){
-        //
+    public function addAddQuotation(Request $request)
+    {
+
+        $validator = Validator::make(
+            [
+                'currency' => $request->post('currency'),
+                'name' => $request->post('name'),
+                'designation' => $request->post('designation'),
+                'department' => $request->post('department'),
+                'kindAttention' => $request->post('kind-attention'),
+                'enquiryNo' => $request->post('enquiry-no'),
+                'quotationNo' => $request->post('quotation-no'),
+                'date' => $request->post('date'),
+                'days' => $request->post('days'),
+                'email' => $request->post('email'),
+                'customer' => $request->post('customer'),
+                'salesPerson' => $request->post('sales-person'),
+            ],
+            [
+                'currency' => 'required',
+                'name' => 'required',
+                'designation' => 'required',
+                'department' => 'required',
+                'kindAttention' => 'required',
+                'enquiryNo' => 'required',
+                // 'quotationNo' => 'required',
+                'date' => 'required',
+                'days' => 'required',
+                'customer' => 'required',
+                // 'salesPerson' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect('admin/quotation')->withErrors($validator)->withInput();
+        }
+        return redirect()->back()->withSuccess("Submit Successfully");
+        // p('success form submit');
     }
 }
--- resources/views/admin/Quotation/add-quotation.blade.php
+++ resources/views/admin/Quotation/add-quotation.blade.php
@@ -7,7 +7,7 @@
 
         <div class="container">
 
-            {{ Form::open(['url' => url('add-quotation'), 'class' => '', 'id' => 'quotationForm']) }}
+            {{ Form::open(['url' => url('admin/add-quotation'), 'class' => '', 'id' => 'quotationForm']) }}
             @csrf
             <div class="row" style="margin-top: 25px;margin-bottom: 25px;">
                 <div class="col-sm-3">
@@ -22,21 +22,22 @@
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="name">To:</label><br>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name"
+                            value="{{ old('name') }}">
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="designation">DESIGNATION:</label><br>
                         <input type="text" class="form-control" id="designation" name="designation"
-                            placeholder="Enter Designation">
+                            placeholder="Enter Designation" value="{{ old('designation') }}">
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="department">DEPARTMENT:</label><br>
                         <input type="text" class="form-control" id="department" name="department"
-                            placeholder="Enter Department">
+                            placeholder="Enter Department" value="{{ old('department') }}">
                     </div>
                 </div>
             </div>
@@ -47,21 +48,21 @@
                     <div class="form-group">
                         <label for="kind-attention">KIND ATTENTION:</label><br>
                         <input type="text" class="form-control" id="kind-attention" name="kind-attention"
-                            placeholder="Enter Name">
+                            placeholder="Enter Name" value="{{ old('kind-attention') }}">
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="enquiry-no">ENQUIRY NO:</label><br>
                         <input type="number" class="form-control" id="enquiry-no" name="enquiry-no"
-                            placeholder="ENQUIRY NO">
+                            placeholder="ENQUIRY NO" value="{{ old('enquiry-no') }}">
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="quotation-no">QUOTATION NO:</label><br>
                         <input type="number" class="form-control" id="quotation-no" name="quotation-no"
-                            placeholder="QUOTATION NO" disabled>
+                            placeholder="QUOTATION NO" disabled value="{{ old('quotation-no') }}">
                     </div>
                 </div>
                 <div class="col-sm-3">
@@ -88,18 +89,33 @@
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
-                        <label for="customar">CUSTOMER:</label><br>
-                        <input type="text" class="form-control" id="customar" name="customar" placeholder="Name">
+                        <label for="customer">CUSTOMER:</label><br>
+                        <input type="text" class="form-control" id="customer" name="customer" placeholder="Name"
+                            value="{{ old('customer') }}">
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
                         <label for="sales-person">SALES PERSON :</label><br>
+                        {{-- <select id="sales-person" name="sales-person" class="form-control">
+                            <option value="" selected disabled>SELECT EMPLOYEE</option>
+                        </select> --}}
+
                         <select id="sales-person" name="sales-person" class="form-control">
                             <option value="" selected disabled>SELECT EMPLOYEE</option>
+                            @if ($allCustomer)
+                                @foreach ($allCustomer as $k => $customer)
+                                    <option value="{{ $customer }}">{{ $customer }}</option>
+                                @endforeach
+                            @endif
                         </select>
                     </div>
                 </div>
+                {{-- <div class="col-sm-4">
+                    <div class="form-group">
+                        <input type="submit" value="Submit">
+                    </div>
+                </div> --}}
             </div>
             {{ Form::close() }}
 
