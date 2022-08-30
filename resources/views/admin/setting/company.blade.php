057289c9840b83e90d59578cd8198f9337729f33
subham-codesigma <subham.ghorui@codesigma.tech>
2022-08-30 22:05:56 +0530

    fix

--- resources/views/admin/setting/company.blade.php
+++ resources/views/admin/setting/company.blade.php
@@ -1,123 +1,122 @@
 @extends('layouts.admin-template')
+
 @section('content')
+
     {!! showMessage() !!}
-    <div class="panel panel-flat">
-        <div class="container">
-            <div class="contact-form-two">
-                <div class="sec-title">
-                    <span class="border-bottom">
-                        <h2>Add Company</h2>
-                    </span>
-                </div>
-                {{ Form::open(['url' => url('admin/add-company'), 'class' => 'row col-offset-md-3', 'id' => '']) }}
-                @csrf
-                <div class="row form-group">
-                    <div class="col-sm-12 form-group">
+
+    <div class="panel">
+        <div class="panel-heading">
+            <h3 class="panel-title text-bold">Add Company</h3>
+{{--            <div class="heading-elements">--}}
+{{--                <a href="{{ url('admin/service') }}" class="btn btn-primary">List Service</a>--}}
+{{--            </div>--}}
+        </div>
+        <div class="panel-body">
+            {{ Form::open(['url' => url('admin/add-company'), 'class' => 'row col-offset-md-3', 'id' => '']) }}
+            <div class="row">
+                <div class="col-sm-12 form-group">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" class="form-control" name="name" id="name" placeholder="Enter Company Name">
                     @error('name')
-                        <p class="text-danger">{{ $message }}</p>
+                    <p class="text-danger">{{ $message }}</p>
                     @enderror
                 </div>
-                    <div class="col-sm-12 form-group">
+                <div class="col-sm-12 form-group">
                     <label for="address" class="form-label">Address</label>
                     <input type="text" class="form-control" name="address" id="address"
-                        placeholder="Enter Company Address">
+                           placeholder="Enter Company Address">
                     @error('address')
-                        <p class="text-danger">{{ $message }}</p>
+                    <p class="text-danger">{{ $message }}</p>
                     @enderror
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 form-group">
+                    <label for="country" class="form-label">Country</label>
+                    <input type="text" class="form-control" name="country" id="country"
+                           placeholder="Enter Company Country">
+                    @error('country')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
                 </div>
-                <div class="row form-group ">
-                    <div class="col-sm-3">
-                        <label for="country" class="form-label">Country</label>
-                        <input type="text" class="form-control" name="country" id="country"
-                            placeholder="Enter Company Country">
-                        @error('country')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="col-sm-3">
-                        <label for="state" class="form-label">State</label>
-                        <input type="text" class="form-control" name="state" id="state"
-                            placeholder="Enter Company State">
-                        @error('state')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="col-sm-3">
-                        <label for="city" class="form-label">City</label><br>
-                        <select name="city" id="city" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>Kolkata</option>
-                            <option>Mumbai</option>
-                            <option>Delhi</option>
-                            <option>Chennai</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-3">
-                        <label for="pin" class="form-label">PIN</label>
-                        <input type="text" class="form-control" name="pin" id="pin" placeholder="Enter PIN No">
-                        @error('pin')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                        <samp>
-                    </div>
+                <div class="col-sm-3 form-group">
+                    <label for="state" class="form-label">State</label>
+                    <input type="text" class="form-control" name="state" id="state"
+                           placeholder="Enter Company State">
+                    @error('state')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
                 </div>
-                <div class="row form-group">
-                    <div class="col-sm-4">
-                        <label for="contact" class="form-label">Contact No.</label>
-                        <input type="text" class="form-control" name="contact" id="contact"
-                            placeholder="Enter Company Contact No">
-                        @error('contact')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="col-sm-4">
-                        <label for="email" class="form-label">Email Address</label>
-                        <input type="email" class="form-control" name="email" id="email"
-                            placeholder="Enter Company Email Address">
-                        @error('email')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
+                <div class="col-sm-3 form-group">
+                    <label for="city" class="form-label">City</label><br>
+                    <select name="city" id="city" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>Kolkata</option>
+                        <option>Mumbai</option>
+                        <option>Delhi</option>
+                        <option>Chennai</option>
+                    </select>
                 </div>
-                <div class="row form-group">
-                    <div class="col-sm-4">
-                        <label for="gstin" class="form-label">GSTIN</label>
-                        <input type="test" class="form-control" name="gstin" id="gstin"
-                            placeholder="Enter Company GSTIN">
-                        @error('gstin')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="col-sm-4">
-                        <label for="pan" class="form-label">PAN No.</label>
-                        <input type="text" class="form-control" name="pan" id="pan"
-                            placeholder="Enter PAN No.">
-                        @error('pan')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="col-sm-4">
-                        <label for="url" class="form-label">Company URL</label>
-                        <input type="text" class="form-control" name="url" id="url"
-                            placeholder="Enter Company URL">
-                        @error('url')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
+                <div class="col-sm-3 form-group">
+                    <label for="pin" class="form-label">PIN</label>
+                    <input type="text" class="form-control" name="pin" id="pin" placeholder="Enter PIN No">
+                    @error('pin')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
                 </div>
-                <div class="row form-group">
-                    <div class="col-sm-12 form-group">
+            </div>
+            <div class="row">
+                <div class="col-sm-4 form-group">
+                    <label for="contact" class="form-label">Contact No.</label>
+                    <input type="text" class="form-control" name="contact" id="contact"
+                           placeholder="Enter Company Contact No">
+                    @error('contact')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="col-sm-8 form-group">
+                    <label for="email" class="form-label">Email Address</label>
+                    <input type="email" class="form-control" name="email" id="email"
+                           placeholder="Enter Company Email Address">
+                    @error('email')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 form-group">
+                    <label for="gstin" class="form-label">GSTIN</label>
+                    <input type="test" class="form-control" name="gstin" id="gstin"
+                           placeholder="Enter Company GSTIN">
+                    @error('gstin')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="col-sm-4 form-group">
+                    <label for="pan" class="form-label">PAN No.</label>
+                    <input type="text" class="form-control" name="pan" id="pan"
+                           placeholder="Enter PAN No.">
+                    @error('pan')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="col-sm-4 form-group">
+                    <label for="url" class="form-label">Company URL</label>
+                    <input type="text" class="form-control" name="url" id="url"
+                           placeholder="Enter Company URL">
+                    @error('url')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 form-group">
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
-                </div>
-                {{ Form::close() }}
             </div>
+            {{ Form::close() }}
         </div>
     </div>
-
     <div class="panel">
         <div class="panel-heading">
             <h3 class="panel-title text-bold">MAIN COMPANIES</h3>
@@ -144,6 +143,8 @@
             </div>
         </div>
     </div>
+
+
 @endsection
 @section('footer_script')
     <link href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" id="theme" rel="stylesheet">
